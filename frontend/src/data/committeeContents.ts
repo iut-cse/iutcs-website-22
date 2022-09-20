fd5ab905364683cb95f8f13df572fd5359e63f7d
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-09-21 02:44:20 +0600

    faculty content added

--- frontend/src/data/committeeContents.ts
+++ frontend/src/data/committeeContents.ts
@@ -1,56 +1,88 @@
+import chiefpatron from "../assets/executives-committee/faculty-body/chief_patron.jpg"
+import patron from "../assets/executives-committee/faculty-body/patron.jpg";
+import chairman from "../assets/executives-committee/faculty-body/chairman.jpg";
+import moderator from "../assets/executives-committee/faculty-body/moderator.jpg";
+import advisorSabbir from "../assets/executives-committee/faculty-body/advisor_sabbir.jpg";
+import advisorFardin from "../assets/executives-committee/faculty-body/advisor_fardin.jpg";
+import advisorImtiaz from "../assets/executives-committee/faculty-body/advisor_imtiaz.jpg";
+import advisorNafisa from "../assets/executives-committee/faculty-body/advisor_nafisa.jpeg";
+import advisorShuvro from "../assets/executives-committee/faculty-body/advisor_shuvro.jpg";
+import advisorRaida from "../assets/executives-committee/faculty-body/advisor_raida.jpg";
+
+import nifty from "../assets/executives-committee/student-body/Nifty.jpg"
+import saju from "../assets/executives-committee/student-body/Saju.jpg"
+import sabbir from "../assets/executives-committee/student-body/Sabbir.jpg"
+import abdullah from "../assets/executives-committee/student-body/Abdullah.jpg"
+import minhaz from "../assets/executives-committee/student-body/Minhaz.jpg";
+
 export const teacherBodyContents = [
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "PROF. DR. MOHAMMAD RAFIQUL ISLAM",
+    position: "CHIEF PATRON",
+    picture: chiefpatron,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "DR OMAR JAH",
+    position: "PATRON",
+    picture: patron,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "PROF DR. ABU RAIHAN MOSTOFA KAMAL",
+    position: "CHAIRMAN",
+    picture: chairman,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "DR. HASAN MAHMUD",
+    position: "MODERATOR",
+    picture: moderator,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "SABBIR AHMED",
+    position: "ADVISOR",
+    picture: advisorSabbir,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "FARDIN SAAD",
+    position: "ADVISOR",
+    picture: advisorFardin,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "IMTIAZ AHMED CHOWDHURY",
+    position: "ADVISOR",
+    picture: advisorImtiaz,
   },
+  {
+    name: "NAFISA SADAF HRITI",
+    position: "ADVISOR",
+    picture: advisorNafisa,
+  },
+  {
+    name: "ALI ABIR SHUVRO",
+    position: "ADVISOR",
+    picture: advisorShuvro,
+  },
+  {
+    name: "MALIHA NOUSHIN RAIDA",
+    position: "ADVISOR",
+    picture: advisorRaida,
+  }
 ];
 
 export const studentBodyContents = [
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "Nifty",
+    position: "President",
+    picture: nifty,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "Saju",
+    position: "Vice President (Finance)",
+    picture: saju,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "Sabbir",
+    position: "Vice President (Management)",
+    picture: sabbir,
   },
   {
     name: "Some Name",
