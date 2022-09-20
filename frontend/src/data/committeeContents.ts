4d95d27984e7fe0464cb2b3d760aff3f0ab496c2
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-09-21 04:00:37 +0600

    student added

--- frontend/src/data/committeeContents.ts
+++ frontend/src/data/committeeContents.ts
@@ -1,4 +1,4 @@
-import chiefpatron from "../assets/executives-committee/faculty-body/chief_patron.jpg"
+import chiefpatron from "../assets/executives-committee/faculty-body/chief_patron.jpg";
 import patron from "../assets/executives-committee/faculty-body/patron.jpg";
 import chairman from "../assets/executives-committee/faculty-body/chairman.jpg";
 import moderator from "../assets/executives-committee/faculty-body/moderator.jpg";
@@ -9,11 +9,36 @@ import advisorNafisa from "../assets/executives-committee/faculty-body/advisor_n
 import advisorShuvro from "../assets/executives-committee/faculty-body/advisor_shuvro.jpg";
 import advisorRaida from "../assets/executives-committee/faculty-body/advisor_raida.jpg";
 
-import nifty from "../assets/executives-committee/student-body/Nifty.jpg"
-import saju from "../assets/executives-committee/student-body/Saju.jpg"
-import sabbir from "../assets/executives-committee/student-body/Sabbir.jpg"
-import abdullah from "../assets/executives-committee/student-body/Abdullah.jpg"
+import nifty from "../assets/executives-committee/student-body/Nifty.jpg";
+import saju from "../assets/executives-committee/student-body/Saju.jpg";
+import sabbir from "../assets/executives-committee/student-body/Sabbir.jpg";
+import abdullah from "../assets/executives-committee/student-body/Abdullah.jpeg";
 import minhaz from "../assets/executives-committee/student-body/Minhaz.jpg";
+import faria from "../assets/executives-committee/student-body/Faria.jpg";
+import musa from "../assets/executives-committee/student-body/musa.jpg";
+import ubada from "../assets/executives-committee/student-body/Ubada.jpg";
+import sadat from "../assets/executives-committee/student-body/Sadat.jpg";
+import mishal from "../assets/executives-committee/student-body/Mishal.jpg";
+import fairuz from "../assets/executives-committee/student-body/Fairuz.jpeg";
+import sheetab from "../assets/executives-committee/student-body/Sheetab.jpeg";
+import abrar from "../assets/executives-committee/student-body/Abrar.jpg";
+import adib from "../assets/executives-committee/student-body/Adib.jpg";
+import ridwan from "../assets/executives-committee/student-body/Nibir.jpg";
+import adiba from "../assets/executives-committee/student-body/Adiba.jpg";
+import taufiqul from "../assets/executives-committee/student-body/Taufiqul.jpg";
+import anuva from "../assets/executives-committee/student-body/Anuva.jpg";
+import jawad from "../assets/executives-committee/student-body/Jawad.jpg";
+import jisan from "../assets/executives-committee/student-body/Jisan.png";
+import nibir from "../assets/executives-committee/student-body/Nibir.jpg";
+import niloy from "../assets/executives-committee/student-body/Niloy.jpeg";
+import sadman from "../assets/executives-committee/student-body/Sadman.jpeg";
+import moshiur from "../assets/executives-committee/student-body/Moshiur.jpeg";
+import sajid from "../assets/executives-committee/student-body/Sajid.jpeg";
+import sananda from "../assets/executives-committee/student-body/Sananda.jpg";
+import sartaj from "../assets/executives-committee/student-body/Sartaj.jpg";
+import shihab from "../assets/executives-committee/student-body/Shihab.jpeg";
+import tawrat from "../assets/executives-committee/student-body/Tawrat.jpg";
+import nafiz from "../assets/executives-committee/student-body/Nafiz.jpg";
 
 export const teacherBodyContents = [
   {
@@ -80,43 +105,154 @@ export const teacherBodyContents = [
 
 export const studentBodyContents = [
   {
-    name: "Nifty",
+    name: "ADHAM ARIK RAHMAN",
     position: "President",
     picture: nifty,
   },
   {
-    name: "Saju",
+    name: "MD HASAN SAJU",
     position: "Vice President (Finance)",
     picture: saju,
   },
   {
-    name: "Sabbir",
+    name: "MD SABBIR RAHMAN",
     position: "Vice President (Management)",
     picture: sabbir,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "MINHAZ KAMAL",
+    position: "TREASURER",
+    picture: minhaz,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "CHOWDHURY MOHAMMAD ABDULLAH",
+    position: "MODERATOR (STUDENT BODY)",
+    picture: abdullah,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "FARIA BINTE KADER",
+    position: "VOLUNTEER COORDINATOR (CSE)",
+    picture: faria,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "MOSHIUR RAFI ALVI",
+    position: "VOLUNTEER COORDINATOR (SWE)",
+    picture: moshiur,
   },
   {
-    name: "Some Name",
-    position: "Some position",
-    picture: "https://dummyimage.com/200x200",
+    name: "SHIHAB SIKDER",
+    position: "EVENT MANAGEMENT EXECUTIVE (CTF)",
+    picture: shihab,
+  },
+  {
+    name: "NAFIZ ZAMAN",
+    position: "EVENT MANAGEMENT EXECUTIVE   (E-SPORTS)",
+    picture: nafiz,
+  },
+  {
+    name: "JISAN ANAM",
+    position: "EVENT MANAGEMENT EXECUTIVE (HACKATHON)",
+    picture: jisan,
+  },
+  {
+    name: "MD SAJID ALTAF",
+    position: `EVENT MANAGEMENT EXECUTIVE (IT BUSINESS CASE STUDY)`,
+    picture: sajid,
+  },
+  {
+    name: "SYED MOHAMMAD SARTAJ EKRAM",
+    position: `EVENT MANAGEMENT EXECUTIVE (MACHINE LEARNING)`,
+    picture: sartaj,
+  },
+  {
+    name: "TAWRATUR RASHID",
+    position: `EVENT MANAGEMENT EXECUTIVE (OLYMPIAD)`,
+    picture: tawrat,
+  },
+  {
+    name: "TAWFIQUL ALAM",
+    position: `EVENT MANAGEMENT EXECUTIVE (PROGRAMMING)`,
+    picture: taufiqul,
+  },
+  {
+    name: "SADMAN SADAT",
+    position: `EVENT MANAGEMENT EXECUTIVE (SEMINAR & WORKSHOP)`,
+    picture: sadman,
+  },
+  {
+    name: "SAMIHA SHAFIQ ANUVA",
+    position: `OPERATIONS EXECUTIVE DESIGN(CSE)`,
+    picture: anuva,
+  },
+  {
+    name: "FATEMA-TUZ-ZOHARA-SANANDA",
+    position: `OPERATIONS EXECUTIVE DESIGN(SWE)`,
+    picture: sananda,
+  },
+  {
+    name: "JAWAD CHOWDHURY",
+    position: `OPERATIONS EXECUTIVE LOGISTICS`,
+    picture: jawad,
+  },
+  {
+    name: "MD ASEER AHMED NILOY",
+    position: `OPERATIONS EXECUTIVE PHOTOGRAPHY & DIGITAL MEDIA`,
+    picture: niloy,
+  },
+  {
+    name: "ADIBA HASAN",
+    position: `OPERATIONS EXECUTIVE PUBLIC RELATION`,
+    picture: adiba,
+  },
+  {
+    name: "MOHAMMAD ABRAR CHOWDHURY",
+    position: `OPERATIONS EXECUTIVE SPONSORSHIP AND LIAISON`,
+    picture: abrar,
+  },
+  {
+    name: "RIDWAN MAHBUB",
+    position: `PUBLICATIONS EXECUTIVE`,
+    picture: ridwan,
+  },
+  {
+    name: "ADIB ABRAR KABEER",
+    position: `WEB DEVELOPMENT EXECUTIVE`,
+    picture: adib,
+  },
+  {
+    name: "HAFIZ SHEETAB",
+    position: `WEB DEVELOPMENT EXECUTIVE`,
+    picture: sheetab,
+  },
+  {
+    name: "MD TOSHADUC RAHMAN",
+    position: `EXECUTIVE SECRETARY PRIZE MANAGEMENT`,
+    picture: nibir,
+  },
+  {
+    name: "FAIRUZ SHAIARA",
+    position: `EXECUTIVE SECRETARY RECEPTION`,
+    picture: fairuz,
+  },
+  {
+    name: "MISHAL AL RAHMAN",
+    position: `EXECUTIVE SECRETARY REFRESHMENT`,
+    picture: mishal,
+  },
+  {
+    name: "ABU JOBAYER MD SADAT",
+    position: `EXECUTIVE SECRETARY TRANSPORT`,
+    picture: sadat,
+  },
+  {
+    name: "UBADA ABBAS BAO IBRAHIM",
+    position: `INTERNATIONAL STUDENT EXECUTIVE (SUDAN)`,
+    picture: ubada,
+  },
+  {
+    name: "MUSA S JAWO",
+    position: `INTERNATIONAL STUDENT EXECUTIVE (THE GAMBIA)`,
+    picture: musa,
   },
 ];
+
